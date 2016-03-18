712fb46b8612c6e4ca5f895ef7461bb94a558551
Jonathan Slabaugh <jonathan.slabaugh@gmail.com>
2016-03-18 22:29:39 +0900

    Initial Commit

---
+++ src/Exor/Phpgmaps/Facades/Phpgmaps.php
@@ -0,0 +1,14 @@
+<?php namespace Exor\Phpgmaps\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Phpgmaps extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'phpgmaps'; }
+
+}
---
+++ src/Exor/Phpgmaps/PhpgmapsServiceProvider.php
@@ -0,0 +1,52 @@
+<?php namespace Exor\Phpgmaps;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+
+class PhpgmapsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('Exor/phpgmaps');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        $this->app->booting(function()
+        {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('Gmaps', 'Exor\Phpgmaps\Facades\Phpgmaps');
+        });
+		$this->app['phpgmaps'] = $this->app->share(function($app){
+            return new Phpgmaps();
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('phpgmaps');
+	}
+
+}
